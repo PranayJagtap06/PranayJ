b82b5f82e6d5f54812fea9c2e97c617c00c011df
Pranay Jagtap <66717457+PranayJagtap06@users.noreply.github.com>
2024-06-12 10:43:45 +0000

    updated toggle theme

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,13 +1,25 @@
 const modeToggle = document.getElementById('mode-toggle');
+const modeIcon = document.getElementById('mode-icon');
 const body = document.body;
 
 modeToggle.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
+  updateModeIcon();
 });
 
 window.addEventListener('DOMContentLoaded', () => {
   const isDarkMode = localStorage.getItem('darkMode') === 'true';
   body.classList.toggle('dark-mode', isDarkMode);
+  updateModeIcon();
 });
 
+function updateModeIcon() {
+  if (body.classList.contains('dark-mode')) {
+    modeIcon.classList.remove('fa-moon');
+    modeIcon.classList.add('fa-sun');
+  } else {
+    modeIcon.classList.remove('fa-sun');
+    modeIcon.classList.add('fa-moon');
+  }
+}
